9ce8e39404956de9e49935fc682958b4ad6ced1a
VOOleksandrov22 <vooleksandrov22@codingburgas.bg>
2024-04-22 21:07:42 +0300

    Update .gitignore in order cmake to work properly

---
+++ Qt/msvc2019_64/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=D:\Qt\6.7.0\msvc2019_64\bin;%PATH%
+cd /D D:\Qt\6.7.0\msvc2019_64
+echo Remember to call vcvarsall.bat to complete environment setup!
